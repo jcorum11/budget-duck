eecabc59299650716b75c7c0eaa17457e476113f
Jacob Corum <cor15031@byui.edu>
2020-09-07 08:28:59 -0600

    fix on service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,20 +3,20 @@ const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./js/idb.js",
-  "./js/index.js",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png",
-  "./css/styles.css"
+  "/",
+  "index.html",
+  "manifest.json",
+  "js/idb.js",
+  "js/index.js",
+  "icons/icon-72x72.png",
+  "icons/icon-96x96.png",
+  "icons/icon-128x128.png",
+  "icons/icon-144x144.png",
+  "icons/icon-152x152.png",
+  "icons/icon-192x192.png",
+  "icons/icon-384x384.png",
+  "icons/icon-512x512.png",
+  "css/styles.css"
 ];
 
 self.addEventListener('install', function (e) {
