0b90ed372bbded01af9e0991509186e1b2b77c23
Jacob Corum <cor15031@byui.edu>
2020-09-07 08:15:41 -0600

    fix on service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,34 @@
-const APP_PREFIX = "BudgetDuck-";
+const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/idb.js",
-  "./public/index.js",
-  "./public/icon-128x128.png",
-  "./public/icon-144x144.png",
-  "./public/icon-152x152.png",
-  "./public/icon-192x192.png",
-  "./public/icon-384x384.png",
-  "./public/icon-512x512.png",
-  "./public/icon-72x72.png",
-  "./public/icon-96x96.png",
-  "./public/styles.css"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./js/idb.js",
+  "./js/index.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./css/styles.css"
 ];
 
-self.addEventListener("activate", function (e) {
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+});
+
+self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
@@ -28,7 +39,7 @@ self.addEventListener("activate", function (e) {
       return Promise.all(
         keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
-            console.log("deleting cache : " + keyList[i]);
+            console.log('deleting cache : ' + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
@@ -37,22 +48,20 @@ self.addEventListener("activate", function (e) {
   );
 });
 
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
-        return request;
+        console.log('responding with cache : ' + e.request.url)
+        return request
       } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
-        return fetch(e.request);
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
       }
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+      //You can omit if/else for console.log & put one line below like this too
+      //return request || fetch(e.request)
     })
-  );
+  )
 });
